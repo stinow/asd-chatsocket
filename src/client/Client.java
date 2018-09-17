6cd244c86cae72884b0727e9a08a697248789f90
Arthur Cardoso <cardosovha@gmail.com>
2018-09-17 09:57:15 -0300

    #2 - unused imports

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,20 +1,13 @@
 package server;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.Console;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
