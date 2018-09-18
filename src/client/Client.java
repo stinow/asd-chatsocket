0569048578fcb461f1786d78090a4ee48b42daff
Gustavo de Souza <cxzaqwed@users.noreply.github.com>
2018-09-18 01:51:56 -0300

    sistema de arquivos

--- src/client/Client.java
+++ src/client/Client.java
@@ -9,53 +9,70 @@ import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Client
-{
-    public static void main(String[] args)
-    {
-       try {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+        try {
             Socket socket = new Socket("127.0.0.1", 12345);
 
             BufferedReader keyboardInputBR = new BufferedReader(new InputStreamReader(System.in));
             DataOutputStream fromClient = new DataOutputStream(socket.getOutputStream());
-            BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));         
-            
+            BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            File arquivo = new File("/diretorio/" + "nome do arquivo");
+            FileInputStream in = new FileInputStream(arquivo);
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(out);
+            BufferedWriter writer = new BufferedWriter(osw);
+            writer.write(arquivo.getName() + "\n");
+            writer.flush();
+            int tamanho = 4096; // buffer de 4KB  
+            byte[] buffer = new byte[tamanho];
+            int lidos = -1;
+            while ((lidos = in.read(buffer, 0, tamanho)) != -1) {
+                out.write(buffer, 0, lidos);
+            }
+
             //envio
             new InputHandler(keyboardInputBR, fromClient, socket).start();
 
             //recebimento
             new OutputHandler(fromServer, socket).start();
-        } 
-        catch (UnknownHostException e) 
-        {
+        } catch (UnknownHostException e) {
             e.printStackTrace();
-        } 
-        catch (IOException e) 
-        {
+        } catch (IOException e) {
             e.printStackTrace();
-        } 
+        }
     }
 }
 
-class InputHandler extends Thread{
+class InputHandler extends Thread {
+
     BufferedReader keyboardInputBR;
     DataOutputStream fromClient;
     Socket cliente;
-    
-    public InputHandler(BufferedReader br, DataOutputStream os, Socket cliente){
+
+    public InputHandler(BufferedReader br, DataOutputStream os, Socket cliente) {
         this.keyboardInputBR = br;
         this.fromClient = os;
         this.cliente = cliente;
     }
-    
+
     @Override
-    public void run(){
+    public void run() {
         try {
-            while(!this.cliente.isClosed()){
+            while (!this.cliente.isClosed()) {
                 keyboardInputBR = new BufferedReader(new InputStreamReader(System.in));
                 String mensagemEnviar = keyboardInputBR.readLine();
-                
-                if(mensagemEnviar != null){
+
+                if (mensagemEnviar != null) {
                     fromClient.writeBytes(mensagemEnviar + "\n");
                 }
             }
@@ -65,23 +82,24 @@ class InputHandler extends Thread{
     }
 }
 
-class OutputHandler extends Thread{
+class OutputHandler extends Thread {
+
     Socket cliente;
     BufferedReader fromServer;
-    
-    public OutputHandler(BufferedReader br, Socket cliente){
+
+    public OutputHandler(BufferedReader br, Socket cliente) {
         this.fromServer = br;
         this.cliente = cliente;
     }
-    
+
     @Override
-    public void run(){
+    public void run() {
         try {
-            while(!this.cliente.isClosed()){
+            while (!this.cliente.isClosed()) {
                 String mensagemRecebida = fromServer.readLine();
-                if(mensagemRecebida != null){
+                if (mensagemRecebida != null) {
                     System.out.println(mensagemRecebida);
-                    if(mensagemRecebida.toLowerCase().contains("voce acabou de se desconectar")){
+                    if (mensagemRecebida.toLowerCase().contains("voce acabou de se desconectar")) {
                         System.exit(0);
                     }
                 }
